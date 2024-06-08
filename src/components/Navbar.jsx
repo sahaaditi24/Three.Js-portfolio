23d6960d65df6ec11243e869dc848795c8badc5c
sahaaditi24 <sahaaditi925@gmail.com>
2024-06-08 16:09:39 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+        <NavLink to ='/'>
+
+        </NavLink>
+
+    </header>
+  )
+}
+
+export default Navbar
