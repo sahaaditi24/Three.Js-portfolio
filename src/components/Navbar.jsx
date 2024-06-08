6dbf12f247f8453072ca0fb124972e1e9e621f09
sahaaditi24 <sahaaditi925@gmail.com>
2024-06-08 19:49:20 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <header className='header'>
-        <NavLink to ='/'>
+    <header className="header">
+       <NavLink to = "/" className="w-10 h-10 rounded-lg bg-white items-center flex font-bold shadow-md">
+       <p className='blue-gradient_text p-2'>AS</p>
 
-        </NavLink>
-
+       </NavLink>
     </header>
   )
 }
