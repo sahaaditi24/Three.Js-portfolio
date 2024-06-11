429279d17a025206b7b9671f874da83eb1e60a71
sahaaditi24 <sahaaditi925@gmail.com>
2024-06-11 23:23:43 +0530

    okk

--- src/components/Homeinfo.jsx
+++ src/components/Homeinfo.jsx
@@ -1,41 +1,48 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
-
-const InfoBox = ({ text, link, btnText}) =>(
+import {arrow} from '../assets/icons'
+ 
+const InfoBox = ({text, link, btnText}) =>(
     <div className='info-box'>
-        <p className="font-medium sm: text-xl text-center">{text}</p>
-        <Link to = {link}>
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className='neo-brutalism-white neo-btn'>
             {btnText}
+            <img src={arrow} className='w-4 h-4 object-contain'/>
         </Link>
     </div>
 )
 
 const renderContent = {
     1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-           Hi, this is <span className='font-semibold'>Aditi</span> 
-           <br/>
-                A hopeless girl from no-where!
-        </h1>
-        
-    ),
-    2: (
-    <InfoBox
-        text ="Totally demotivated in life"
-        link = "/about"
-        btnTech = "Just surviving" 
-        
-    />
-    ),
-    3: (
-        <h1>3</h1>
-    ),
-    4: (
-        <h1>4</h1>
-    )
-
+       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+       Hi, this is <span className='font-semibold'>Aditi</span>
+       <br />
+       An innocent girl from no-where
+       </h1>  
+      ),
+      2:(
+        <InfoBox
+            text ="depressed soul"
+            link = "/about"
+            btnText = "Trying hard to survive"
+        />
+      ),
+      3: (
+        <InfoBox
+            text ="depressed soul"
+            link = "/about"
+            btnText = "Trying hard to survive"
+        />
+      ),
+      4: (
+        <InfoBox
+            text ="depressed soul"
+            link = "/about"
+            btnText = "Trying hard to survive"
+        />
+      ),
+    
 }
-
 
 
 const Homeinfo = ({currentStage}) => {
