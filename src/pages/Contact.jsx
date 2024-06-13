a18169491a7686005864db2e49bf581a964b00dd
sahaaditi24 <sahaaditi925@gmail.com>
2024-06-13 14:01:47 +0530

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const Contact = () => {
+  const [form, setForm] = useState({name: '', email: '', message: ''})
+
+  const handleChange = () => {};
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1>Get in touch</h1>
+        <form className='w-full flex flex-col gap-7 mt-14'>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type ="text"
+              name="name"
+              className= "input"
+              placeholder= "adie"
+              required value ={form.name}
+              onChange={handleChange}/>
+          </label>
+
+
+        </form>
+      </div>
+
+    </section> 
+
   )
 }
 
