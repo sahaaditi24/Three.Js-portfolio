1c80f5a0d7de7fb827661d6c7b7d6ac0d33eedcf
sahaaditi24 <sahaaditi925@gmail.com>
2024-06-13 14:41:37 +0530

    git

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import {useState} from 'react'
+import {useRef} from 'react'
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({name: '', email: '', message: ''})
+  const [isLoading, setisLoading] = useState(false);
   const handleChange = () => {};
   const handleFocus = () => {};
   const handleBlur = () => {};
+  const handleSubmit = () => {};
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1>Get in touch</h1>
-        <form className='w-full flex flex-col gap-7 mt-14'>
+        <form className='w-full flex flex-col gap-7 mt-14' onSubmit={handleSubmit}>
           <label className='text-black-500 font-semibold'>
             Name
 
@@ -53,11 +57,11 @@ const Contact = () => {
           <label className='text-black-500 font-semibold'>
             Message
 
-            <input 
-              type="text"
+            <textarea 
               name='message'
-              className='input'
-              placeholder="hello"
+              rows = {4}
+              className='textarea'
+              placeholder="Let me know if you need any help"
               required 
               value = {form.message} 
               onChange = {handleChange}
@@ -67,7 +71,11 @@ const Contact = () => {
           />
 
           </label>
-          
+
+          <button type="submit" className='btn' disabled={isLoading} onFocus={handleFocus} onBlur={handleBlur}>
+            {isLoading ? 'Sending...' : 'Send Message'}
+
+          </button>
 
         </form>
       </div>
