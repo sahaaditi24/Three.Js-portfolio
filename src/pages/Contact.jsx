f6700da78a86b8e045cadfd5a94a3f369a4d38cd
sahaaditi24 <sahaaditi925@gmail.com>
2024-06-16 15:13:16 +0530

    ok

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.sendForm(
+    emailjs.send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
@@ -27,6 +27,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       ).then (() =>{
           setIsLoading(false);
+          setForm({ name:'', email:'', message:''});
       }).catch((error) => {
           setIsLoading(false);
           console.log(error);
